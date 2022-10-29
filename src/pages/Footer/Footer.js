622a7ef16d5580b44c883b70adda456c3dae70cf
Brian Mason <brian.kw.mason@gmail.com>
2022-10-29 15:05:51 -0400

    Made home page fully responsive

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -3,30 +3,33 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-    return (
-        <div className={styles.footerContainer}>
-            <div className={styles.left}>
-                <div className={styles.email}>
-                    <a
-                        className={styles.link}
-                        href="mailto:climamobileapp@gmail.com"
-                    >
-                        Contact
-                    </a>
-                </div>
-                <div className={styles.privacy}>
-                    <a
-                        className={styles.link}
-                        href="https://clima-privacy-policy.netlify.app/"
-                        target="_blank"
-                    >
-                        Privacy
-                    </a>
-                </div>
-            </div>
-            <div className={styles.copyright}>&copy; 2022 Clima</div>
+  return (
+    <div className={styles.footerContainer}>
+      <div className={styles.left}>
+        <div className={styles.email}>
+          <a
+            className={styles.link}
+            href="mailto:climamobileapp@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Contact
+          </a>
         </div>
-    );
+        <div className={styles.privacy}>
+          <a
+            className={styles.link}
+            href="https://clima-privacy-policy.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Privacy
+          </a>
+        </div>
+      </div>
+      <div className={styles.copyright}>&copy; 2022 Clima</div>
+    </div>
+  );
 };
 
 export default Footer;

3244f52685637a008fd18be5c63bd81560a3387e
Brian Mason <brian.kw.mason@gmail.com>
2022-11-08 18:05:45 -0500

    Added sharing functionality

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import Benefits from "./pages/BenefitsPage/Benefits";
 import Locations from "./pages/LocationPage/Locations";
 import "./App.css";
 import Footer from "./pages/Footer/Footer";
+import React, { useEffect } from "react";
+import { isAndroid, isIOS } from "react-device-detect";
 
 function App() {
-    return (
-        <div className="App">
-            <Home />
-            <Benefits />
-            <Locations />
-            <Footer />
-        </div>
-    );
+  useEffect(() => {
+    if (isAndroid) {
+      window.location.href =
+        "https://play.google.com/store/apps/details?id=com.clima.mobile.app";
+    } else if (isIOS) {
+      window.location.href =
+        "https://apps.apple.com/us/app/clima-save-the-environment/id6444138849";
+    }
+  }, []);
+  return (
+    <div className="App">
+      <Home />
+      <Benefits />
+      <Locations />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -8,67 +8,57 @@ import appStore from "../../assets/svgs/appstore.svg";
 import googlePlay from "../../assets/svgs/googleplay.svg";
 
 const Home = () => {
-    return (
-        <div className={styles.homePageContainer}>
-            <div
-                className={styles.backgroundImage}
-                style={{
-                    backgroundImage: `url(${environment})`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                }}
-            ></div>
-            <div className={styles.contentContainer}>
-                <div
-                    className={`${styles.left} animate__animated animate__fadeInLeft`}
-                >
-                    <div className={styles.content}>
-                        <h1 className={styles.header}>Clima</h1>
-                        <h2 className={styles.subHeader}>
-                            Gamify reductions in carbon emissions
-                        </h2>
-                        <div className={styles.downloadImages}>
-                            <a href="#">
-                                <img
-                                    src={appStore}
-                                    alt="Download on app store"
-                                    className={styles.download}
-                                />
-                            </a>
-                            <a href="#">
-                                <img
-                                    src={googlePlay}
-                                    alt="Download on app store"
-                                    className={styles.download}
-                                />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div
-                    className={`${styles.right} animate__animated animate__fadeInRight`}
-                >
-                    <img
-                        src={homePageSS}
-                        alt="App home page"
-                        className={styles.phone}
-                    />
-                </div>
-            </div>
-
-            <a
-                className={`${styles.downButton} animate__animated animate__pulse`}
-                href="#benefits"
-            >
+  return (
+    <div className={styles.homePageContainer}>
+      <div
+        className={styles.backgroundImage}
+        style={{
+          backgroundImage: `url(${environment})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></div>
+      <div className={styles.contentContainer}>
+        <div className={`${styles.left} animate__animated animate__fadeInLeft`}>
+          <div className={styles.content}>
+            <h1 className={styles.header}>Clima</h1>
+            <h2 className={styles.subHeader}>
+              Gamify reductions in carbon emissions
+            </h2>
+            <div className={styles.downloadImages}>
+              <a href="https://apps.apple.com/us/app/clima-save-the-environment/id6444138849">
                 <img
-                    className={styles.downArrow}
-                    src={arrow}
-                    alt="Go to Features"
+                  src={appStore}
+                  alt="Download on app store"
+                  className={styles.download}
                 />
-            </a>
+              </a>
+              <a href="https://play.google.com/store/apps/details?id=com.clima.mobile.app">
+                <img
+                  src={googlePlay}
+                  alt="Download on app store"
+                  className={styles.download}
+                />
+              </a>
+            </div>
+          </div>
         </div>
-    );
+        <div
+          className={`${styles.right} animate__animated animate__fadeInRight`}
+        >
+          <img src={homePageSS} alt="App home page" className={styles.phone} />
+        </div>
+      </div>
+
+      <a
+        className={`${styles.downButton} animate__animated animate__pulse`}
+        href="#benefits"
+      >
+        <img className={styles.downArrow} src={arrow} alt="Go to Features" />
+      </a>
+    </div>
+  );
 };
 
 export default Home;
